274ce3ae86805b02276478dbb29969117e74dcad
foyarekhua <fatimahoyarekhua@gmail.com>
2021-10-14 16:20:48 -0400

    removed map attribute as I believe it belongs in class Deck. added attribute currentCard to keep track of what card the user is working with. Added method shuffle cards.

--- src/StudySession.java
+++ src/StudySession.java
@@ -1,2 +1,23 @@
-public interface StudySession {
+import java.util.Collections;
+import java.util.Map;
+
+// TODO documentation
+public class StudySession {
+    public Deck deck;
+    int currentCard;
+
+    public void createSession(Deck deck) {
+        this.deck = deck;
+    }
+
+    public Flashcard getNextCard(){
+        this.currentCard = currentCard + 1;
+        return deck.getFlashcards()[this.currentCard - 1];
+    }
+
+    public void shuffleCards(){
+        Collections.shuffle(this.deck);
+        currentCard = 0;
+    }
+
 }
