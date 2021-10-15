dd9278dd166249b0cbe55a9bd9d3acee2c76a95d
SirRAL <76802978+SirRAL@users.noreply.github.com>
2021-10-15 01:53:32 -0400

    Implement FlashcardInteractor and PracticeSession, and privatize Flashcard's instance attributes

--- src/Flashcard.java
+++ src/Flashcard.java
@@ -1,12 +1,12 @@
 
 public class Flashcard {
-    String front;
-    String back;
+    private String front;
+    private String back;
 
     /**
      * Construct a Flashcard entity with a front and back.
-     * @param front A string representing the text at the front of a Flashcard
-     * @param back A string representing the text at the back of a Flashcard
+     * @param front The front of a Flashcard
+     * @param back The back of a Flashcard
      */
     public Flashcard(String front, String back) {
         this.front = front;
@@ -15,7 +15,7 @@ public class Flashcard {
 
     /**
      * Set the front of this Flashcard to newFront.
-     * @param newFront A string of what to set this Flashcard's front to.
+     * @param newFront What to set this Flashcard's front to.
      */
     public void setFront(String newFront) {
         this.front = newFront;
@@ -23,7 +23,7 @@ public class Flashcard {
 
     /**
      * Set the back of this Flashcard to newBack.
-     * @param newBack A string of what to set this Flashcard's back to.
+     * @param newBack What to set this Flashcard's back to.
      */
     public void setBack(String newBack) {
         this.back = newBack;
@@ -31,7 +31,7 @@ public class Flashcard {
 
     /**
      * Return the front of this Flashcard.
-     * @return The (String) front of this card.
+     * @return The front of this card.
      */
     public String getFront() {
         return this.front;
@@ -39,7 +39,7 @@ public class Flashcard {
 
     /**
      * Return the back of this Flashcard.
-     * @return The (String) back of this card.
+     * @return The back of this card.
      */
     public String getBack() {
         return this.back;
--- src/PracticeSession.java
+++ src/PracticeSession.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
 public class PracticeSession implements StudySession {
+    private Deck deck;
+    private Map<Flashcard, Integer> proficiencies;
+
+    /**
+     * Construct a PracticeSession instance that initializes all proficiencies to 0.
+     * @param deck The deck that this session will use to show cards to the user.
+     */
+    public PracticeSession(Deck deck) {
+        this.deck = deck;
+        for (Flashcard card : this.deck.flashcards) {
+            this.proficiencies.put(card, 0);
+        }
+    }
+
+    /**
+     * Randomly pick a new card from this PracticeSession's deck to show to the user.
+     * @return Return a randomly chosen Flashcard from this PracticeSession's deck.
+     */
+    public Flashcard getNextCard() {
+        Random random = new Random();
+        ArrayList<Flashcard> flashcard_lst = this.deck.flashcards;
+        int rand_index = random.nextInt();
+        return flashcard_lst.get(rand_index);
+    }
+
 }
