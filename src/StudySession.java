a94f85adcab7257c492a4b03d143e52b6c7bb1b1
foyarekhua <fatimahoyarekhua@gmail.com>
2021-10-16 14:11:03 -0400

    fixed map -> hashmap

--- src/StudySession.java
+++ src/StudySession.java
@@ -1,11 +1,12 @@
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 // TODO documentation
 public class StudySession {
     // TODO consider changing some of these private
     public Deck deck;
-    public Map<Flashcard, Integer> proficiencies = new Map<Flashcard, Integer>();
+    public Map<Flashcard, Integer> proficiencies = new HashMap<>();
     public String name;
     int currentCard;
 
@@ -19,13 +20,13 @@ public class StudySession {
         this.name = "Untitled";
     }
 
-    public Flashcard getNextCard(){
+    public Flashcard getNextCard() {
         this.currentCard = currentCard + 1;
         return deck.getFlashcards()[this.currentCard];
     }
 
-    public void shuffleCards(){
-        Collections.shuffle(this.deck);
+    public void shuffleCards() {
+        Collections.shuffle(this.deck.getFlashcards());
         currentCard = 0;
     }
 
