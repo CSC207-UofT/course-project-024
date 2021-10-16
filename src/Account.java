02d81d904d4c8fa1c4a1f325558e9547fe38966e
Mahathi <gmahathi02@gmail.com>
2021-10-16 19:51:03 -0400

    Implemented Account and AccountInteractor

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Account {
+    public String username;
+    public String password;
+    public List<Deck> decks;
+
+    public Account(String username, String password, List<Deck> decks){
+        this.username = username;
+        this.password = password;
+        this.decks = decks;
+    }
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.decks = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String newUsername){
+        username = newUsername;
+    }
+
+    public boolean isCorrectPassword(String attemptedPassword){
+        return Objects.equals(password, attemptedPassword);
+    }
 }
