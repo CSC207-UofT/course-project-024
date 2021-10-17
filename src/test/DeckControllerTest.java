28a3a2433fa4f1019856c96466b55f4ffc134430
Jason Wang <78438775+Ixstreme@users.noreply.github.com>
2021-10-16 22:08:58 -0400

    Finish DeckControllerTest

--- src/test/DeckControllerTest.java
+++ src/test/DeckControllerTest.java
@@ -5,16 +5,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DeckControllerTest {
     DeckController deckController;
+    Account account;
 
     @BeforeEach
     void setUp() {
         deckController = new DeckController();
-        Account account = AccountInteractor.createAccount("user1", "pass1");
+        account = AccountInteractor.createAccount("user1", "pass1");
+    }
+
+    @Test
+    void addCard() {
+        Deck deck = deckController.createDeck("Deck Name");
+        deckController.addCard(deck, "front", "back");
+        // TODO: check if card exists in account's Deck-StudySession proficiency map
+        assertEquals(1, deck.getFlashcards().size());
+    }
+
+    @Test
+    void deleteCard() {
+        Deck deck = deckController.createDeck("Deck Name");
+        deckController.addCard(deck, "front", "back");
+        deckController.deleteCard(deck, deck.getFlashcards().get(0));
+        assertEquals(0, deck.getFlashcards().size());
+    }
+
+    @Test
+    void renameDeck() {
+        Deck deck = deckController.createDeck("Deck Name");
+        deckController.renameDeck(deck, "New name");
+        assertEquals("New name", deck.getName());
     }
 
     @Test
     void createDeck() {
-        Deck deck = deckController.createDeck(account);
-        assertTrue(AccountInteractor.getDecks(account).contains(deck));
+        Deck deck = deckController.createDeck("Deck Name");
+        // TODO: check if account has deck
+        assertTrue(deckController.decks.contains(deck));
     }
 }
