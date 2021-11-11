d705b4e313e125f05e13e92b57f058c0d5d69be8
SirRAL <76802978+SirRAL@users.noreply.github.com>
2021-11-11 17:52:51 -0500

    Create FlashcardData class to replace proficiency

---
+++ src/main/FlashcardData.java
@@ -0,0 +1,60 @@
+public class FlashcardData {
+    private int proficiency;
+    private int cardsUntilDue;
+    private int consecutiveCorrects;
+    private int consecutiveIncorrects;
+    private boolean isSeen;
+
+    public FlashcardData(int defaultCardsUntilDue) {
+        this.isSeen = false;
+        this.proficiency = 0;
+        this.cardsUntilDue = defaultCardsUntilDue;
+        this.consecutiveCorrects = 0;
+        this.consecutiveIncorrects = 0;
+    }
+
+    public boolean getIsSeen() {
+        return this.isSeen;
+    }
+
+    public int getProficiency() {
+        return proficiency;
+    }
+
+    public int getCardsUntilDue() {
+        return cardsUntilDue;
+    }
+
+    public int getConsecutiveCorrects() {
+        return consecutiveCorrects;
+    }
+
+    public int getConsecutiveIncorrects() {
+        return consecutiveIncorrects;
+    }
+
+    public void setCardsUntilDue(int newCardsUntilDue) {
+        this.cardsUntilDue = newCardsUntilDue;
+    }
+
+    public void resetConsecutiveCorrects() {
+        this.consecutiveCorrects = 0;
+    }
+
+    public void resetConsecutiveIncorrects() {
+        this.consecutiveIncorrects = 0;
+    }
+
+    public void incrementConsecutiveCorrects() {
+        this.consecutiveCorrects += 1;
+    }
+
+    public void incrementConsecutiveIncorrects() {
+        this.consecutiveIncorrects += 1;
+    }
+
+    public void incrementProficiency(int delta) {
+        this.proficiency += delta;
+    }
+
+}
