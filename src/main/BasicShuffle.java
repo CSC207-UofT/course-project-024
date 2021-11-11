787a1fb2934a5b0522b43acb2f4e5cd3c45e0802
SirRAL <76802978+SirRAL@users.noreply.github.com>
2021-11-11 17:54:31 -0500

    Refactor PracticeSession to use BasicShuffle

---
+++ src/main/BasicShuffle.java
@@ -0,0 +1,43 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class BasicShuffle implements CardShuffler {
+
+    int counter = 0;
+    LinkedList<Flashcard> linkedListDeck;
+
+    public BasicShuffle(Map<Flashcard, FlashcardData> flashcardData) {
+        // This turns an ImmutableList, which ToList returns, to a LinkedList. Don't ask me why.
+        this.linkedListDeck = new LinkedList<Flashcard>(flashcardData.keySet().stream().toList());
+    }
+
+    @Override
+    public void shuffleCards() {
+        Collections.shuffle(this.linkedListDeck);
+    }
+
+    @Override
+    public Flashcard returnChosenFlashcard() {
+        if (counter == 0) {
+            System.out.println("First run!");
+            shuffleCards();
+            counter += 1;
+            return linkedListDeck.getFirst();
+        } else if (counter < linkedListDeck.size()) {
+            System.out.println("Removing first and adding to last");
+            Flashcard oldFirstFlashcard = linkedListDeck.getFirst();
+            linkedListDeck.removeFirst();
+            linkedListDeck.addLast(oldFirstFlashcard);
+            counter += 1;
+            return linkedListDeck.getFirst();
+        } else { // counter == flashcardData.size()
+            System.out.println("Reached end, shuffling!");
+            Collections.shuffle(linkedListDeck);
+            counter = 0;
+            return linkedListDeck.getFirst();
+
+        }
+
+    }
+}
