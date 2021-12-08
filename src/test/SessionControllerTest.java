3e83b38da95715c74f6c0c0ece78cce5108cd815
Jason Wang <78438775+Ixstreme@users.noreply.github.com>
2021-12-08 03:22:33 -0500

    Amend SessionControllerTest

--- src/test/SessionControllerTest.java
+++ src/test/SessionControllerTest.java
@@ -1,72 +1,100 @@
-import Accounts.Account;
-import Decks.Deck;
-import Decks.DeckController;
+import Accounts.AccountDTO;
+import Accounts.AccountInteractor;
 import Decks.DeckDTO;
 import Decks.DeckInteractor;
-import Flashcards.Flashcard;
+import Flashcards.FlashcardDTO;
+import Flashcards.FlashcardDataDTO;
 import Sessions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessionControllerTest {
 
-    DeckController deckController;
-    Account account;
     SessionController sessionController;
-    StudySessionDTO session;
-    DeckDTO deck;
 
     @BeforeEach
     void setUp() {
-//        this.deckController = new DeckController();
-        this.account = new Account("hi", "bye");
+        AccountDTO accountDTO = AccountInteractor.createAccount("user", "pass");
+        AccountInteractor.login(accountDTO, "pass");
         this.sessionController = new SessionController();
-        this.deckController.createDeck("default");
+        DeckDTO deck = DeckInteractor.createDeck("default");
+        AccountInteractor.addDeckToCurrentAccount(deck);
+        AccountInteractor.selectDeck(deck);
+        DeckInteractor.addFlashcardToCurrentDeck("front1", null, "back1");
+        DeckInteractor.addFlashcardToCurrentDeck("front2", null, "back2");
     }
 
     @Test
     void startPracticeSession() {
-        this.deck = DeckInteractor.getCurrentDeck();
-//        this.sessionController.startPracticeSession(this.deck);
-        StudySession nonDTOsession = SessionInteractor.convertDTOToSession(this.session);
-        Deck nonDTOdeck = DeckInteractor.convertDTOToDeck(this.deck);
-        assertTrue(this.account.getDecksToSessions().get(nonDTOdeck).contains(nonDTOsession));
+        sessionController.startSession(DeckInteractor.getCurrentDeck(), PracticeSessionDTO.class);
+        DeckDTO deck = null;
+        Map<DeckDTO, List<StudySessionDTO>> decksToSessions = AccountInteractor.getCurrentAccount().getDecksToSessions();
+        Set<DeckDTO> decks = decksToSessions.keySet();
 
+        for (DeckDTO d : decks) {
+            if (d.getName().equals("default")) {
+                deck = d;
+            }
+        }
+        assertTrue(decksToSessions.get(deck).stream().anyMatch(
+                s -> s.getClass().equals(PracticeSessionDTO.class)));
     }
 
     @Test
     void startLearningSession() {
-        this.deck = DeckInteractor.getCurrentDeck();
-//        this.sessionController.startLearningSession(this.deck);
-        StudySession nonDTOsession = SessionInteractor.convertDTOToSession(this.session);
-        Deck nonDTOdeck = DeckInteractor.convertDTOToDeck(this.deck);
+        sessionController.startSession(DeckInteractor.getCurrentDeck(), LearningSessionDTO.class);
+        DeckDTO deck = null;
+        Map<DeckDTO, List<StudySessionDTO>> decksToSessions = AccountInteractor.getCurrentAccount().getDecksToSessions();
+        Set<DeckDTO> decks = decksToSessions.keySet();
 
-        assertTrue(this.account.getDecksToSessions().get(nonDTOdeck).contains(nonDTOsession));
+        for (DeckDTO d : decks) {
+            if (d.getName().equals("default")) {
+                deck = d;
+            }
+        }
+        assertTrue(decksToSessions.get(deck).stream().anyMatch(
+                s -> s.getClass().equals(LearningSessionDTO.class)));
+    }
+
+    @Test
+    void startTestSession() {
+        sessionController.startSession(DeckInteractor.getCurrentDeck(), TestSessionDTO.class);
+        DeckDTO deck = null;
+        Map<DeckDTO, List<StudySessionDTO>> decksToSessions = AccountInteractor.getCurrentAccount().getDecksToSessions();
+        Set<DeckDTO> decks = decksToSessions.keySet();
+
+        for (DeckDTO d : decks) {
+            if (d.getName().equals("default")) {
+                deck = d;
+            }
+        }
+        assertTrue(decksToSessions.get(deck).stream().anyMatch(
+                s -> s.getClass().equals(TestSessionDTO.class)));
     }
 
     @Test
     void getNextCard() {
-        this.deck = DeckInteractor.getCurrentDeck();
-//        this.sessionController.startPracticeSession(this.deck);
-        this.deckController.addCard("1", null, "1");
+        this.sessionController.startSession(DeckInteractor.getCurrentDeck(), PracticeSessionDTO.class);
         assertNotNull(this.sessionController.getNextCard());
     }
 
     @Test
     void postAnswerUpdate() {
-        this.deck = DeckInteractor.getCurrentDeck();
-//        this.sessionController.startLearningSession(this.deck);
-        this.session = SessionInteractor.getCurrentSession();
-        StudySession nonDTOsession = SessionInteractor.convertDTOToSession(this.session);
-        this.deckController.addCard("1", null, "1");
-        this.deckController.addCard("2", null, "2");
-        Flashcard currFlashcard = nonDTOsession.getNextCard();
+        sessionController.startSession(DeckInteractor.getCurrentDeck(), PracticeSessionDTO.class);
+        FlashcardDTO flashcard = this.sessionController.getNextCard();
         this.sessionController.postAnswerUpdate(true);
-        assertTrue(nonDTOsession.getFlashcardToData().get(currFlashcard).getCardsUntilDue() > 0);
-        currFlashcard = nonDTOsession.getNextCard();
-        this.sessionController.postAnswerUpdate(false);
-        assertTrue(nonDTOsession.getFlashcardToData().get(currFlashcard).getCardsUntilDue() > 0);
+        Map<FlashcardDTO, FlashcardDataDTO> flashcardToData = SessionInteractor.getCurrentSession().getCardShufflerDTO().getFlashcardToData();
+
+        Set<FlashcardDTO> flashcardDTOs = flashcardToData.keySet();
+        for (FlashcardDTO f : flashcardDTOs) {
+            if (f.getFrontText().equals(flashcard.getFrontText())) {
+                flashcard = f;
+            }
+        }
+        assertTrue(flashcardToData.get(flashcard).getCardsUntilDue() > 0);
     }
 }
